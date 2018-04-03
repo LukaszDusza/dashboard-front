c25cf4caa8d9876c5e2a98c6b5c2dd040390e6ad
lukaszdusza <lukaszdusza280@gmail.com>
2018-04-03 20:03:25 +0200

    03-04-2018

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,65 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+// --- BOOTSTRAP ---
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+// --------ANGULAR MATERIAL-----------------
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+//import {MatCheckboxModule} from '@angular/material/checkbox';
+//import {MatMenuModule} from '@angular/material/menu';
+//import {MatButtonModule} from '@angular/material/button';
+//import {MatIconModule} from '@angular/material/icon';
+//import {MatDatepickerModule} from '@angular/material/datepicker';
+//import { DatePickerModule} from 'angular-material-datepicker';
+//import { MaterialModule } from '@angular/material';
+// -------END ANGULAR MATERIAL--------------
+
+import { NavigationModule } from './modules/navigation/navigation.module';
+import { SnapshotModule } from './modules/snapshot/snapshot.module';
+import { AppRoutingModule } from './app.routing.module';
+import { HomeModule } from './modules/home/home.module';
+import { ReportsModule } from './modules/reports/reports.module';
+import { MainService } from './main-service.service';
+import { HttpClientModule } from '@angular/common/http';
+import { ChartsModule } from './modules/charts/charts.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    //-- BOOTSTRAP --
+    NgbModule.forRoot(),
+    FormsModule,
+    ReactiveFormsModule,
+    //--ANGULAR MATERIAL--   
+    BrowserAnimationsModule,
+   // DatePickerModule,
+   // MatCheckboxModule,
+   // MatMenuModule,
+   // MatButtonModule,
+   // MatIconModule,
+   // MatDatepickerModule,  
+    //--END ANGULAR MATERIAL--
+    
+    // -- PAGE MODULES --
+    NavigationModule,
+    SnapshotModule,
+    HomeModule,
+    ReportsModule,
+    ChartsModule,
+    HttpClientModule,
+    
+  //-- ROUTERS --
+  AppRoutingModule,
+  // -- HTTP --
+  
   ],
-  providers: [],
+  providers: [MainService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
